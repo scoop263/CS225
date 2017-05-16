6e174fe349a9f9a83da7451bede38583847b6679
scoop263 <sdeditius1995@gmail.com>
2017-05-15 23:02:35 -0700

    Example to using Traps

---
+++ lab10.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+TEMPFILE=$(mktemp -t lab10.XXXXXXXXXX)
+trap cleanup HUP INT QUIT TERM
+cleanup () {
+echo "You sir have interrupted the program!"
+rm -f $TEMPFILE
+exit 1 ;}
+i=0
+for i in $(find / ); do
+   echo $i >> $TEMPFILE
+done
